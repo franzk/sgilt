255a4b20b7408c546f81989aa6bd1e9dd916377b
François Koehler <francois.koehler@lde.fr>
2024-12-08 22:16:33 +0100

    bg image

--- sgilt-front/src/views/HomeView.vue
+++ sgilt-front/src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
-  <HomeSearchBox class="home-search-box" />
-  <HomePartnerSection class="home-partner-section" />
+  <div class="home-view">
+    <HomeSearchBox class="home-search-box" />
+    <HomePartnerSection class="home-partner-section" />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,13 +11,21 @@ import HomePartnerSection from '@/components/HomePartnerSection.vue'
 </script>
 
 <style lang="scss" scoped>
-.home-search-box {
-  margin-top: 150px;
-}
-.home-partner-section {
-  background-color: white;
-  margin-top: 100px;
-  padding-left: 1rem;
-  padding-right: 1rem;
+.home-view {
+  background: url('@/assets/images/home_bg.png');
+  box-shadow: inset 0 0 0 2000px rgba(200, 200, 200, 0.5);
+  display: flex;
+  flex-direction: column;
+
+  .home-search-box {
+    margin-top: 180px;
+    background-color: white;
+  }
+  .home-partner-section {
+    background-color: white;
+    margin-top: 80px;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
 }
 </style>
