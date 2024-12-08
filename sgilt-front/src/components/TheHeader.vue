620dda3934653e2e8c42c32844c3346f1c8985c0
François Koehler <francois.koehler@lde.fr>
2024-12-09 00:28:30 +0100

    couleurs

--- sgilt-front/src/components/TheHeader.vue
+++ sgilt-front/src/components/TheHeader.vue
@@ -9,13 +9,20 @@
 
 <style lang="scss" scoped>
 header {
-  padding-left: 1rem;
-  background-color: #eee;
-  border-bottom: 1px solid #ccc;
+  position: sticky;
+  top: 0;
+  left: 0;
+
+  background-color: $color-light;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
   display: flex;
   flex: 1;
   gap: 2rem;
   align-content: center;
+  flex-wrap: wrap;
+
+  padding-left: 1rem;
 
   p {
     padding: 0;
