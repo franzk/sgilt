b272126fdddae9e30bd812bf006b93b66b4abf56
François Koehler <francois.koehler@lde.fr>
2024-12-18 23:59:35 +0100

    amélioration chips

--- sgilt-front/src/components/basics/CategoryChip.vue
+++ sgilt-front/src/components/basics/CategoryChip.vue
@@ -1,14 +1,24 @@
 <template>
   <SgiltChip :label="$t(`categories.${category}`)">
-    <CategoryIcon :category="category" class="icon" />
+    <component :is="cmp" class="icon" />
   </SgiltChip>
 </template>
 
 <script setup lang="ts">
 import SgiltChip from '@/components/basics/SgiltChip.vue'
-import CategoryIcon from '@/components/basics/CategoryIcon.vue'
+import IconFood from '@/components/icons/IconFood.vue'
+import IconMusic from '@/components/icons/IconMusic.vue'
+import IconPlace from '@/components/icons/IconPlace.vue'
+import IconPhoto from '@/components/icons/IconPhoto.vue'
 
-defineProps<{
-  category: string
+const props = defineProps<{
+  category: 'food' | 'music' | 'place' | 'photography'
 }>()
+
+const cmp = {
+  food: IconFood,
+  music: IconMusic,
+  place: IconPlace,
+  photography: IconPhoto,
+}[props.category]
 </script>
