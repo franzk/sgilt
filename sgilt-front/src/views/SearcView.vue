e81411615bfe8dfd6e5c3f695683a68255e7f80a
François Koehler <francois.koehler@lde.fr>
2024-12-20 14:25:44 +0100

    create search view

---
+++ sgilt-front/src/views/SearcView.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="search-view">
+    <div class="search-bar">
+      <p>SEARCH</p>
+    </div>
+    <div class="search-results">
+      <p>RESULTS</p>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.search-view {
+  display: flex;
+  flex-direction: row;
+
+  .search-bar {
+    flex: 0 0 20rem;
+    background-color: $shadow;
+    padding: $spacing-m;
+  }
+
+  .search-results {
+    flex: 1;
+    background-color: $color-secondary;
+    padding: $spacing-m;
+  }
+}
+</style>
