f8a710d008831c5df439f93c13fcfe67e5ead7c3
François Koehler <francois.koehler@lde.fr>
2025-03-04 00:39:29 +0100

    correction

--- sgilt-front/src/data/repository/EventActivityRepository.ts
+++ sgilt-front/src/data/repository/EventActivityRepository.ts
@@ -13,7 +13,7 @@ const eventActivities: EventActivity[] = [
     id: '1',
     date: new Date('2025-02-24T10:15:00'),
     partnerName: 'Food Truck Burgers',
-    partnerAvatarUrl: './images/food-truck-burgers.jpg',
+    partnerAvatarUrl: '/images/food-truck-burgers.jpg',
     eventActivityTypeId: 'reservation-sent',
     payload: {
       message: 'Votre demande a bien été envoyée au partenaire.',
@@ -23,7 +23,7 @@ const eventActivities: EventActivity[] = [
     id: '2',
     date: new Date('2025-02-24T11:00:00'),
     partnerName: 'DJ Animation',
-    partnerAvatarUrl: './images/dj-animation.jpg',
+    partnerAvatarUrl: '/images/dj-animation.jpg',
     eventActivityTypeId: 'reservation-viewed',
     payload: {
       message: 'Le partenaire a vu votre demande de réservation.',
@@ -33,7 +33,7 @@ const eventActivities: EventActivity[] = [
     id: '3',
     date: new Date('2025-02-24T12:30:00'),
     partnerName: 'Gypsy Reed Ensemble',
-    partnerAvatarUrl: './images/jazz-band.jpg',
+    partnerAvatarUrl: '/images/jazz-band.jpg',
     eventActivityTypeId: 'message-received',
     payload: {
       message: "Bonjour, pouvez-vous préciser le lieu de l'événement ?",
@@ -43,7 +43,7 @@ const eventActivities: EventActivity[] = [
     id: '4',
     date: new Date('2025-02-24T14:45:00'),
     partnerName: 'Food Truck Burgers',
-    partnerAvatarUrl: './images/food-truck-burgers.jpg',
+    partnerAvatarUrl: '/images/food-truck-burgers.jpg',
     eventActivityTypeId: 'reservation-approved',
     payload: {
       message: 'Le partenaire a accepté votre demande.',
@@ -53,7 +53,7 @@ const eventActivities: EventActivity[] = [
     id: '5',
     date: new Date('2025-02-24T16:10:00'),
     partnerName: 'DJ Animation',
-    partnerAvatarUrl: './images/dj-animation.jpg',
+    partnerAvatarUrl: '/images/dj-animation.jpg',
     eventActivityTypeId: 'payment-required',
     payload: {
       amount: '150€',
@@ -64,7 +64,7 @@ const eventActivities: EventActivity[] = [
     id: '6',
     date: new Date('2025-02-24T17:30:00'),
     partnerName: 'Gypsy Reed Ensemble',
-    partnerAvatarUrl: './images/jazz-band.jpg',
+    partnerAvatarUrl: '/images/jazz-band.jpg',
     eventActivityTypeId: 'payment-confirmed',
     payload: {
       amount: '200€',
@@ -75,7 +75,7 @@ const eventActivities: EventActivity[] = [
     id: '7',
     date: new Date('2025-02-24T18:00:00'),
     partnerName: 'DJ Animation',
-    partnerAvatarUrl: './images/dj-animation.jpg',
+    partnerAvatarUrl: '/images/dj-animation.jpg',
     eventActivityTypeId: 'message-sent',
     payload: {
       message: 'Merci pour votre réponse ! On valide la prestation ?',
@@ -85,7 +85,7 @@ const eventActivities: EventActivity[] = [
     id: '8',
     date: new Date('2025-02-24T18:45:00'),
     partnerName: 'Food Truck Burgers',
-    partnerAvatarUrl: './images/food-truck-burgers.jpg',
+    partnerAvatarUrl: '/images/food-truck-burgers.jpg',
     eventActivityTypeId: 'reservation-updated',
     payload: {
       message: 'Le partenaire a mis à jour sa prestation.',
@@ -95,7 +95,7 @@ const eventActivities: EventActivity[] = [
     id: '9',
     date: new Date('2025-02-24T19:20:00'),
     partnerName: 'DJ Animation',
-    partnerAvatarUrl: './images/dj-animation.jpg',
+    partnerAvatarUrl: '/images/dj-animation.jpg',
     eventActivityTypeId: 'reservation-canceled',
     payload: {
       message: 'Votre réservation a été annulée.',
@@ -105,7 +105,7 @@ const eventActivities: EventActivity[] = [
     id: '10',
     date: new Date('2025-02-24T20:00:00'),
     partnerName: 'Food Truck Burgers',
-    partnerAvatarUrl: './images/food-truck-burgers.jpg',
+    partnerAvatarUrl: '/images/food-truck-burgers.jpg',
     eventActivityTypeId: 'partner-unavailable',
     payload: {
       message: 'Le partenaire n’est plus disponible à cette date.',
