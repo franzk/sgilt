5c88907461bdf3afd0c9017c4eafc80b8548b4b0
Franz Ka <koehler.francois@gmail.com>
2026-07-29 22:35:50 +0200

    fix decoration subcatÃ

--- sgilt-front/app/utils/constants.ts
+++ sgilt-front/app/utils/constants.ts
@@ -45,7 +45,7 @@ export const APP_CATEGORIES: Category[] = [
     key: 'services',
     name: 'Services',
     subcategories: [
-      { key: '', name: 'Décoration', categoryKey: 'services' },
+      { key: 'Décoration', name: 'Décoration', categoryKey: 'services' },
       { key: 'location-lieu', name: 'Location de matériel', categoryKey: 'services' },
     ],
   },
